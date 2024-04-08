28e050ffc7cfab308fbb33690e90d29c188885c1
Aquamoris <pestryak.nikita.dev@gmail.com>
2024-04-08 17:42:58 +1000

    Add validation

--- form.ts
+++ form.ts
@@ -1,9 +1,13 @@
-import styles from './AddTechnicForm.module.scss';
 import {Controller, SubmitErrorHandler, SubmitHandler, useForm} from "react-hook-form";
 
 type AddForm = {
     name: string;
     age: number;
+    cardNumber: number;
+}
+
+const normalizeCardNumber = (value: string) => {
+    return value.replace(/\s/g, "").match(/.{1,4}/g)?.join(" ").substr(0, 19) || "";
 }
 
 const AddTechnicForm = () => {
@@ -15,15 +19,17 @@ const AddTechnicForm = () => {
         setValue,
         control, // Интеграция с библиотеками
         clearErrors,
-        formState: { errors }
+        formState: { errors, isValid }
     } = useForm<AddForm>({
         defaultValues: {
             age: 18
-        }
+        },
+        mode: "onBlur"
     });
     
     const submit: SubmitHandler<AddForm> = data => {
-        console.log(data);
+        alert(JSON.stringify(data));
+        reset(); // Очистка формы
     }
 
     const error: SubmitErrorHandler<AddForm> = data => {
@@ -33,16 +39,45 @@ const AddTechnicForm = () => {
     return (
         <>
             <form onSubmit={handleSubmit(submit, error)}>
-                <input type="text" {...register('name', {required: true})} aria-invalid={!!errors.name}/>
+
+                <label>
+                    Имя
+                <input type="text" {...register('name', {
+                    required: 'Поле обязательно',
+                    minLength: {
+                        value: 5,
+                        message: 'Длина должна быть больше 5 символов'
+                    }
+                })} aria-invalid={!!errors.name}/>
+                </label>
+                <div>{errors?.name && <p>{errors?.name?.message || 'Ошибка'}</p>}</div>
+
                 <Controller render={({ field }) => <input {...field} />} name='age' control={control} />
+
                 <input type="number" {...register('age')}/>
-                <button>Отправить</button>
+
+                <input
+                    placeholder='0000 0000 0000 0000'
+                    type='tel'
+                    inputMode='numeric'
+                    autoComplete='cc-number'
+                    {...register('cardNumber')}
+                    onChange={(event) => {
+                        const {value} = event.target;
+                        event.target.value = normalizeCardNumber(value);
+                    }}
+                />
+
+                <button disabled={!isValid}>Отправить</button>
+
                 <button type='button' onClick={() => reset({
                     name: '',
                     age: 0
                 })}>Очистить форму</button>
+                
                 <button type='button' onClick={() => clearErrors()}>Очистить ошибки</button>
                 <button type='button' onClick={() => setValue('name', 'Вася')}>Установить имя</button>
+
             </form>
             {watch('age')}
         </>
